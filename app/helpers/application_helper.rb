8e68cb9fdbfcc7c2f57ee852ee1ab29ec5d80d6b
webapp <nicholas.n3y@gmail.com>
2017-01-18 04:38:48 -0500

    added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title ona per-page basis.
+
+  def full_title(page_title = '')
+    base_title = "Foo Ah Corp."
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
